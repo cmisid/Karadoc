0414aceb3e85bb7cf99bc7576650120aef0f35b9
Axel Bellec <axel.bellec@outlook.fr>
2016-11-09 19:22:30 +0100

    implement face_recognition into ShotsProcessor class

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 from preprocessing.metadata import MetadataProcessor
+from preprocessing.shots import ShotsProcessor
 
 
 processor = MetadataProcessor()
 processor.run()
+
+# processor = ShotsProcessor()
+# processor.run()
--- preprocessing/shots.py
+++ preprocessing/shots.py
@@ -1,7 +1,10 @@
 import os
 
 import click
+from collections import namedtuple
+import cv2
 import pandas as pd
+
 
 from .base import Base
 
@@ -12,14 +15,52 @@ class ShotsProcessor(Base):
 
     def __init__(self, input_path='data/shots', output_path='features/shots'):
         super(ShotsProcessor, self).__init__(
-            file_token='.jpg',
+            file_token='*.xml',
             input_path=input_path,
             output_path=output_path
         )
+        self.cascade_classifier_path = os.path.join('data', 'classifiers')
+
+    def shots_img(self):
+        """ Generator of image path """
+        for folder, sub_folder, files in os.walk(self.input_path):
+            for file in files:
+                file_path = os.path.join(folder, file)
+                if file_path.endswith('.jpg'):
+                    yield file_path
+
+    def compute_feature_detection(self, shot=None, cascade_classifier='haarcascade_frontalface_default.xml', scale_factor=1.3, min_neighbors=5):
+        """ Detect faces in the image """
+
+        # Read image
+        img = cv2.imread(shot)
+
+        # Convert image to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # This loads the face cascade into memory so it’s ready for use. Remember,
+        # the cascade is just an XML file that contains the trained data to detect faces.
+        cascade = cv2.CascadeClassifier(os.path.join(
+            self.cascade_classifier_path, cascade_classifier))
+
+        # Detect faces in the image
+        faces = cascade.detectMultiScale(gray, scale_factor, min_neighbors)
+
+        Shot = namedtuple('slot', ['filename', 'shot', 'nb_faces'])
+
+        face_text = '{0} face'.format(len(faces)) if len(
+            faces) == 1 else '{0} faces'.format(len(faces))
+
+        click.secho('{} --- {}'.format(face_text, shot), fg='yellow')
+
+        return Shot(filename=os.path.basename(os.path.split(shot)[0]), shot=os.path.basename(shot), nb_faces=len(faces))
 
     def parse(self, doc):
         pass
 
     def run(self):
-        for shot in self.shots_img:
-            print(shot)
+
+        # Extract the number of faces for each shot
+        nb_faces = [self.compute_feature_detection(shot) for shot in self.shots_img()]
+        faces_df = pd.DataFrame(nb_faces)
+        faces_df.to_csv(os.path.join(self.output_path, 'faces_df.csv'))
