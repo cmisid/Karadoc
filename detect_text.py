735f48cbb8d06b6d9d6cfd8e62434ee9d7370a7c
Axel Bellec <axel.bellec@outlook.fr>
2016-11-16 13:34:34 +0100

    add a TOC into README.md

--- detect_text.py
+++
@@ -1,7 +0,0 @@
-import pytesseract
-from PIL import Image
-
-image_path = 'data/shots/Anglicantv-FCA2009ArchbishopVenables921/00371.jpg'
-
-text = pytesseract.image_to_string(Image.open(image_path))
-print(text)
